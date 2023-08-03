b24b0491af6c502c1ca22fa4865cd4ab541c4cd1
Habaru <evanadhiarjayohanes@gmail.com>
2023-08-03 21:34:10 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+let timeOut = 3000;
+let slideIndex = 0;
+let autoOn = true;
+
+autoSlide();
+
+function autoSlide() {
+    timeOut = timeOut - 20;
+
+            if (autoOn == true && timeOut < 0) {
+                showSlides();
+            }
+            setTimeout(autoSlide, 20);
+}
+
+function prevSlide() {
+
+    timeOut = 2000;
+
+    const slides = document.getElementsByClassName("banner");
+    const dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex--;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    if (slideIndex == 0) {
+        slideIndex = 3
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+function showSlides() {
+
+    timeOut = 2000;
+
+    const slides = document.getElementsByClassName("banner");
+    const dots = document.getElementsByClassName("dot");
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slideIndex++;
+
+    if (slideIndex > slides.length) {
+        slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+const menuBtn = document.getElementsByClassName('menu-button');
+const menu = document.getElementsByClassName('menu');
+const iBtn = document.getElementsByClassName('fa-bars')
+
+function menuFunction() {
+    console.log(menu)
+    menu.classList.toggle("active");
+    iBtn.classList.toggle('active');    
+}
